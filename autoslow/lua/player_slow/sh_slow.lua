b32c36f6ba2886e9a1300bc66f7ff58fe0723236
xbread <bread@geteden.us>
2020-08-31 11:17:42 -0400

    📦 0.1
     - Player speed is now properly calculated and will correctly apply a stun to the player no matter what speed their class has.
     - Players can no longer be stunned twice causing an issue that would glitch out the player. 
     - The stun reset timer will run once fixing an issue were if a player was constantly being hit their speed would decrease, increase, then decrease again.

--- autoslow/lua/player_slow/sh_slow.lua
+++ autoslow/lua/player_slow/sh_slow.lua
@@ -4,20 +4,26 @@
 * For: b2r.online
 * Usage: Backend code for the slow script
 */
+local b_is_stunned = false
 
 function calculate_slow(target, dmg_info)
     if target:IsPlayer() and IsValid(target) and not dmg_info:IsExplosionDamage() and not dmg_info:IsFallDamage() and not dmg_info:GetInflictor():IsPlayer() then
-        target:SetRunSpeed(target:GetRunSpeed() / slow_config.deduct_run)
-        target:SetWalkSpeed(target:GetWalkSpeed() / slow_config.deduct_walk)
+        if not b_is_stunned then
+            target:SetRunSpeed(target:GetRunSpeed() / slow_config.deduct_run)
+            target:SetWalkSpeed(target:GetWalkSpeed() / slow_config.deduct_walk)
+            b_is_stunned = true
+        end
 
-        if slow_config.chatprint then 
+        if slow_config.chatprint then
             target:ChatPrint("[B2R]: You have been stunned by a " .. dmg_info:GetInflictor():GetClass() .. " for " .. slow_config.slowtime .. " seconds")
-        end 
+        end
 
+        if not b_is_stunned then return end
 
         timer.Simple(slow_config.slowtime, function()
-            target:SetWalkSpeed(GAMEMODE.Config.walkspeed)
-            target:SetRunSpeed(GAMEMODE.Config.runspeed)
+            target:SetRunSpeed(target:GetRunSpeed() * slow_config.deduct_run)
+            target:SetWalkSpeed(target:GetWalkSpeed() * slow_config.deduct_walk)
+            b_is_stunned = false
         end)
     end
 end
